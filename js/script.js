25c92c502f32f4b8f751dbf3d8a1eac058538723
sayed <szasohel@gmail.com>
2017-12-23 18:31:44 -0500

    feat: fading added

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(function() {
+	// ************** Fading *************************//
+
+	// $(".class-name").fadeOut(time in ms to fadeOut);
+	$(".red-box").fadeOut(500);
+
+	// $(".class-name").fadeOut(time in ms to fadeIn);
+	$(".blue-box").fadeIn(500);
+
+	// fadeTo: to fade an element to a certain opacity
+	//$(".class-name").fadeTo(time in ms to fadeIn,opacity);
+	$(".green-box").fadeTo(500,.5);
+
+	// fadeToggle: to fadeIn or fadeOut an element from their
+	// previous state
+	//$(".class-name").fadeToggle();
+	$(".green-box").fadeToggle();
+
+	// ************** Show and Hide *******************//
+
+	// ************** Sliding ************************//
+
+	// ************** Moving *************************//
+
+	// ************** Custom animation ***************//
+
+});
