5e0b670eee9ae96c438c6757aad0c123a9ad5baf
Sayed Sohel <szasohel@gmail.com>
2017-12-23 19:00:46 -0500

    feat: basic and sliding effects added

--- js/script.js
+++ js/script.js
@@ -16,9 +16,39 @@ $(function() {
 	//$(".class-name").fadeToggle();
 	$(".green-box").fadeToggle();
 
+	//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
+	// never use fadeTo after using fadeOut on an element
+	// because fadeOut makes the element attribute 
+	// display: hidden. in this case fadeTo can't show the
+	// element back again
+	//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
+
 	// ************** Show and Hide *******************//
 
+	// here time is optional. if we want some effect while
+	// showing, hiding elements we can use time
+
+	// $(".class-name").show();
+	// $(".class-name").show(time in ms to show);
+	$(".red-box").show();
+
+	// $(".class-name").hide();
+	// $(".class-name").show(time in ms to show);
+	$(".red-box").hide();
+
+	// $(".class-name").toggle();
+	// $(".class-name").show(time in ms to show);
+	$(".red-box").toggle();
+
 	// ************** Sliding ************************//
+	// $(".class-name").slideDown(time in ms to slideDown);
+	$(".red-box").slideDown(2000);
+
+	// $(".class-name").slideUp(time in ms to slideUp);
+	$(".red-box").slideUp(2000);
+
+	// $(".class-name").slideToggle(time in ms to slideToggle);
+	$(".red-box").slideToggle(2000);
 
 	// ************** Moving *************************//
 
